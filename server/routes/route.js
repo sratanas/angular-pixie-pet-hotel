f184e18392fe343c3af7e0c4ebaaf5f211121c24
gaglianojoseph <gagli012@umn.edu>
2017-11-29 13:58:13 -0600

    adding additional get and post requests server side

--- server/routes/route.js
+++ server/routes/route.js
@@ -11,8 +11,8 @@ router.get('/', function (req, res) { // get request called on ready and when yo
             res.sendStatus(500);
         } else {
             //connected to database
-            db.query(`SELECT owners.first_name, owners.last_name, owners.id, pets.pet_name, pets.breed, pets.color  FROM pets
-            JOIN owners ON pets.owner_id = owners.id;`, function (errorMakingQuery, result) {
+            db.query(`SELECT owners.first_name, owners.last_name, owners.id, pets.pet_name, pets.breed, pets.color, pets.id  FROM pets
+            LEFT JOIN owners ON pets.owner_id = owners.id;`, function (errorMakingQuery, result) {
                 done();
                 if (errorMakingQuery) {
                     console.log('error making query', errorMakingQuery);
@@ -44,7 +44,7 @@ router.get('/users', function (req, res) { //get request will be called when you
     });
 });
 
-router.post('/', function (req, res) {
+router.post('/new', function (req, res) {
     pool.connect(function (err, db, done) {
         if (err) {
             //error connecting to database
@@ -64,6 +64,29 @@ router.post('/', function (req, res) {
         }
     });
 });
+
+router.post('/users', function (req, res) {
+    pool.connect(function (err, db, done) {
+        if (err) {
+            //error connecting to database
+            console.log('there was an error connecting: ', err);
+            res.sendStatus(500);
+        } else {
+            //connected to database
+            db.query(`INSERT INTO owners("first_name", "last_name")
+            VALUES ($1,$2)`,[req.body.firstName, req.body.lastName], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('error making query', errorMakingQuery);
+                } else {
+                    res.sendStatus(201);
+                }
+            });
+        }
+    });
+});
+
+
 
 // router.put('/:id', function (req, res) {
 //     pool.connect(function (err, db, done) {
